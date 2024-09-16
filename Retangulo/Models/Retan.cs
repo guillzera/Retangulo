cd5772b66ccc2c08a61b1e60ca368006ad5103a2
guillzera <guiguidefarias36@gmail.com>
2024-09-16 19:40:18 -0300

    Area, Diagonal e Perimetro de um Retangulo

---
+++ Retangulo/Models/Retan.cs
@@ -0,0 +1,23 @@
+﻿namespace Retangulo.Models
+{
+    public class Retan
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return Altura * 2 + Largura * 2;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+    }
+}
---
+++ Retangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Retangulo.Models;
+using System.Globalization;
+
+Retan retangulo = new Retan();
+
+Console.WriteLine("Entre a largura e altura do retangulo:");
+retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("AREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PERIMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
